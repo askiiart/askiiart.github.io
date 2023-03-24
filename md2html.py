0e17bb34f2ca91a47b204dfcec6ba2101f6cc658
askiiart <dev@askiiart.n.net>
2023-03-23 22:03:13 -0500

    Add title logic

--- md2html.py
+++ md2html.py
@@ -1,7 +1,6 @@
 import os
 from glob import glob
 
-# pandoc --data-dir /big-stuff/files/websites/personal-site/html --template template.html -t html -o index.html index.md
 os.chdir('/big-stuff/files/websites/personal-site/html')
 md_files = glob('*.md')
 md_files_no_ext = []
@@ -10,4 +9,4 @@ for item in md_files:
     md_files_no_ext.append(item[:-3])
 
 for item in md_files_no_ext:
-    os.system(f'pandoc -f markdown-smart --data-dir /big-stuff/files/websites/personal-site/html --template template.html -t html -o {item}.html {item}.md')
+    os.system(f'pandoc -f markdown-smart --data-dir . --template template.html -t html -o {item}.html {item}.md --metadata title=\"$(grep -m 1 -oP \'(?<=^# ).*\' {item}.md | cat)\"')
