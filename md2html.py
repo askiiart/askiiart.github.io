81291a2c8f782ed14b42ec27ae8e0f3a7fbac095
askiiart <dev@askiiart.net>
2023-05-09 12:33:37 -0500

    Fix stuff

--- md2html.py
+++ md2html.py
@@ -2,8 +2,9 @@ import os
 from pathlib import Path
 
 dir = os.path.dirname(__file__)
+print(dir)
 os.chdir(dir)
 files = [str(item)[:-3] for item in Path(dir).rglob('*.md')]
 
 for item in files:
-    os.system(f'pandoc -f markdown-smart --data-dir . --template template.html -t html -o {item}.html {item}.md --metadata title=\"$(grep -m 1 -oP \'(?<=^# ).*\' {item}.md | cat)\"')
+    os.system(f'pandoc -f markdown-smart --data-dir . --template {dir}/template.html -t html -o {item}.html {item}.md --metadata title=\"$(grep -m 1 -oP \'(?<=^# ).*\' {item}.md | cat)\"')
