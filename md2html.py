faab3a61435cefe099e73f60fb94a13c262b1844
askiiart <dev@askiiart.net>
2023-08-27 18:05:38 -0500

    Add syntax highlighting

--- md2html.py
+++ md2html.py
@@ -8,3 +8,6 @@ files = [str(item)[:-3] for item in Path(dir).rglob('*.md')]
 
 for item in files:
     os.system(f'pandoc -f markdown-smart --data-dir . --template {dir}/template.html -t html -o {item}.html {item}.md --metadata title=\"$(grep -m 1 -oP \'(?<=^# ).*\' {item}.md | cat)\"')
+
+# Replace 'language-' with 'language-' recursively
+os.system("find . \( ! -regex '.*/\..*' \) -type f | xargs sed -i 's/language-/language-/g'")
