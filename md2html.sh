9305040da475b3f1a25158e4b8b8c351abef2d28
askiiart <dev@askiiart.net>
2023-09-11 20:10:21 -0500

    Remove python script with bash script

---
+++ md2html.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+find . -name "*.md" | while read -r item ; do
+    item="${item%.*}"
+    echo "Currently processing ${item}"
+    pandoc -f markdown-smart --data-dir . --template ${dir}/template.html -t html -o ${item}.html ${item}.md --metadata title="$(grep -m 1 -oP '(?<=^# ).*' ${item}.md | cat)"
+done
+
+find . \( ! -regex './md2html.sh' \) -type f | xargs sed -i 's/sourceCode /language-/g'
