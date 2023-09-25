9e3bf5de9f90d75471f4af2595d90c76661266d4
askiiart <dev@askiiart.net>
2023-09-25 14:46:31 -0500

    Skip more stuff

--- md2html.sh
+++ md2html.sh
@@ -15,8 +15,8 @@ done
 # Based off https://en.wikipedia.org/wiki/RSS, particularly the example
 printf "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<rss version=\"2.0\">\n\n<channel>\n  <title>askiiart.net</title>\n  <description>The feed for askiiart.net, I guess</description>\n  <link>https://askiiart.net</link>\n  <lastBuildDate>$(TZ='UTC' date --rfc-2822)</lastBuildDate>" > feed.xml
 find . -name "*.html" | while read -r item; do
-    # Skip template.html
-    if [[ ${item} == "./template.html" ]]; then
+    # Skip template.html, wishlist.html, resume.html, and portfolio.html
+    if [[ ${item} == "./template.html" || ${item} == "./wishlist.html" || ${item} == "./resume.html" || ${item} == "./portfolio.html" ]]; then
         continue
     fi
     item="${item%.*}"
