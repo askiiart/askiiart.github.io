ee00ac3429ab6d71dd217c03adef438a08b2edb2
askiiart <dev@askiiart.net>
2023-10-15 12:38:53 -0500

    Add other RSS feeds

--- generate-rss.sh
+++ generate-rss.sh
@@ -15,3 +15,29 @@ find . -name "*.html" | while read -r item; do
     printf "\n  <item>\n    <title>${TITLE}</title>\n    <link>https://askiiart.net/${item}.html</link>\n  </item>" >>feed.xml
 done
 printf "\n\n</channel>\n</rss>" >>feed.xml
+
+# same thing, but for TIL only
+cd ./til
+printf "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<rss version=\"2.0\">\n\n<channel>\n  <title>askiiart.net TIL</title>\n  <description>The feed for askiiart.net but just TIL, I guess</description>\n  <link>https://askiiart.net/til/</link>\n  <lastBuildDate>$(TZ='UTC' date --rfc-2822)</lastBuildDate>" >feed.xml
+find . -name "*.html" | while read -r item; do
+    # Skip template.html, wishlist.html, resume.html, and portfolio.html
+    item="${item%.*}"
+    item="${item#./}"
+    TITLE=$(grep -m 1 -oP '(?<=^# ).*' ${item}.md | cat)
+    printf "\n  <item>\n    <title>${TITLE}</title>\n    <link>https://askiiart.net/til/${item}.html</link>\n  </item>" >>feed.xml
+done
+printf "\n\n</channel>\n</rss>" >>feed.xml
+cd ..
+
+# same thing, but for blog only
+cd ./blog
+printf "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<rss version=\"2.0\">\n\n<channel>\n  <title>askiiart.net blog</title>\n  <description>The feed for askiiart.net but just the blog, I guess</description>\n  <link>https://askiiart.net/blog/</link>\n  <lastBuildDate>$(TZ='UTC' date --rfc-2822)</lastBuildDate>" >feed.xml
+find . -name "*.html" | while read -r item; do
+    # Skip template.html, wishlist.html, resume.html, and portfolio.html
+    item="${item%.*}"
+    item="${item#./}"
+    TITLE=$(grep -m 1 -oP '(?<=^# ).*' ${item}.md | cat)
+    printf "\n  <item>\n    <title>${TITLE}</title>\n    <link>https://askiiart.net/til/${item}.html</link>\n  </item>" >>feed.xml
+done
+printf "\n\n</channel>\n</rss>" >>feed.xml
+cd ..
