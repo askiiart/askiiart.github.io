b6e41a4f38d17375d2fd6a7b4769e28f9cbf6a78
askiiart <dev@askiiart.net>
2023-11-09 09:29:48 -0600

    Fix img src for errors

--- add-better-http-status-codes.sh
+++ add-better-http-status-codes.sh
@@ -5,3 +5,4 @@ mv pages/ error/
 cp http-status-codes/you.gif assets/
 cp http-status-codes/boykisser.jpg assets/
 rm -rf http-status-codes/
+find error/ -type f | xargs sed -i 's/img src=\\//img src=\https:\/\/askiiart.net\/error\//g'
