e068210ffff82505425eff99fc1df5286b84e59a
askiiart <dev@askiiart.net>
2023-12-05 16:52:03 -0600

    Add conf, update HTTP status codes script

--- add-better-http-status-codes.sh
+++ add-better-http-status-codes.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 rm -rf error/
 git clone https://git.askiiart.net/askiiart/http-status-codes.git
-cp -r http-status-codes/pages/ .
+cd http-status-codes/
+python3 nginx-create-pages.py
+cp -r pages/ ..
+cd ..
 mv pages/ error/
 cp http-status-codes/you.gif assets/
 cp http-status-codes/boykisser.png assets/
